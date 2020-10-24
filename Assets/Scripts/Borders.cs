3c2b03309fda4b5a62265b72ae17836dff69c143
Dry <dry99rus@gmail.com>
2020-10-24 11:34:40 +0000

    Границы

---
+++ Assets/Scripts/Borders.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Borders : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D col)
+	{
+		float forceX = -(col.attachedRigidbody.position.x)/Mathf.Abs(col.attachedRigidbody.position.x);
+		float forceY = -(col.attachedRigidbody.position.y)/Mathf.Abs(col.attachedRigidbody.position.y);
+		Vector2 force = new Vector2 (forceX*5000, forceY*5000);
+		col.attachedRigidbody.AddForce(force); 
+		Debug.Log("border reached");
+	}
+}
