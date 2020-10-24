69784b376caa91684b64ced855a23b4943f012ca
Денис Шуваев <shuden1997@gmail.com>
2020-10-24 08:55:45 +0000

    No Comments

--- Assets/Scripts/Lantern.cs
+++ Assets/Scripts/Lantern.cs
@@ -15,7 +15,12 @@ public class Lantern : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         float distance = Vector3.Magnitude(transform.position - other.gameObject.transform.position);
-        other.gameObject.SendMessage("OnView", distance);
+        if(other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.SendMessage("OnView", distance);
+        }
+
+        
     }
 
     private void OnTriggerExit2D(Collider2D other)
