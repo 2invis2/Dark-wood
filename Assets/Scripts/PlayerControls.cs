7eba391caef62388a502a8e456f9b318a9bc3deb
Денис Шуваев <shuden1997@gmail.com>
2020-10-24 10:50:31 +0000

    Finish Fucking Lantern!

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,13 +6,17 @@ public class PlayerControls : MonoBehaviour
 {
     public float speed = 1;
     private Rigidbody2D rb;
+
     public GameObject lantern;
     public GameObject Sprite_black;
     public GameObject Sprite_color;
     private SpriteRenderer black_render;
     private SpriteRenderer color_renderer;
-	
-	// Start is called before the first frame update
+
+    public GameObject handR;
+    public GameObject handL;
+
+    // Start is called before the first frame update
     void Start()
     {
 		//rb = GetComponent <Rigidbody2D> ();
@@ -55,17 +59,27 @@ public class PlayerControls : MonoBehaviour
         diff.Normalize();
 
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        if(Mathf.Abs(rot_z) > 90)
+        if (Mathf.Abs(rot_z) > 90)
         {
             black_render.flipX = true;
             color_renderer.flipX = true;
+            handL.SetActive(true);
+            handR.SetActive(false);
+
+
         }
         else
         {
             black_render.flipX = false;
             color_renderer.flipX = false;
+            handL.SetActive(false);
+            handR.SetActive(true);
+
         }
+        Debug.Log(rot_z % 90);
+
         lantern.transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+ 
     }
 	
 	public void CaughtByEnemy()
