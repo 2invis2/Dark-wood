59d08352dfc560e7aad3337f334030fc5d2c18a4
Олег Бавыкин <invisible6462@gmail.com>
2020-10-24 06:01:31 +0000

    Добавил коллайдер на фонарик

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private float lightingRange = 1.0f;
+
+    private PolygonCollider2D collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<PolygonCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
