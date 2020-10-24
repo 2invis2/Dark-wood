ffc743eb7066442d83241e46ed31230fe01a5d7d
Денис Шуваев <shuden1997@gmail.com>
2020-10-24 17:39:40 +0000

    No Comments

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameLogic: MonoBehaviour
 {
@@ -10,16 +12,69 @@ public class GameLogic: MonoBehaviour
     private static bool isExitAvilable = false;
 
     public AudioClip backMusic;
+
     private AudioSource audio;
 
+    public Dictionary<string, Sprite> listAllItems = new Dictionary<string, Sprite>();
+
+    public List<string> itemsNames = new List<string>();
+    public List<Sprite> itemsSprites = new List<Sprite>();
+
+    public Text uITextQuestList;
+    public List<Image> questImages;
+
+    public GameObject uIQuestList;
     public void Start()
     {
         audio = GetComponent<AudioSource>();
         audio.PlayOneShot(backMusic);
+
+        InitDictionary();
         TestInit();
+        InitQuestList();
         
+
+
     }
 
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("QuestList"))
+        {
+            Debug.Log(uIQuestList.active);
+            SwitchQuestList();
+        }
+    }
+
+
+    public void SwitchQuestList()
+    {
+        Debug.Log(uIQuestList.active);
+        RefreshQuestList();
+        if (uIQuestList.active)
+        {
+            uIQuestList.SetActive(false);
+
+        }
+        else
+        {
+            uIQuestList.SetActive(true);
+
+        }
+
+
+    }
+    public void InitDictionary()
+    {
+        int i = 0;
+        for(i =0; i<itemsNames.Count; i++)
+        {
+            listAllItems.Add(itemsNames[i], itemsSprites[i]);
+            Debug.Log(itemsNames[i], itemsSprites[i]);
+        }
+        
+    }
     //Временная ручная инициализация списка предметов
     public static void TestInit()
     {
@@ -30,6 +85,43 @@ public class GameLogic: MonoBehaviour
         InitItemList(items);
 
     }
+
+    public void InitQuestList()
+    {
+        string listText = "";
+        int count = 0;
+        foreach (KeyValuePair<string, bool> item in itemsToCollect)
+        {
+            Sprite sp;
+            listText += item.Key + "\n";
+            if (listAllItems.TryGetValue(item.Key, out sp))
+            {
+                questImages[count].enabled = true;
+                questImages[count].sprite = sp;
+            }
+            count += 1;
+
+        }
+        for(int i =count; i <= 5; i++)
+        {
+            listText += "\n";
+        }
+        uITextQuestList.text = listText;
+
+
+    }
+
+    public void RefreshQuestList()
+    {
+        foreach (KeyValuePair<string, bool> item in itemsToCollect)
+        {
+            if ((item.Value)&&(! uITextQuestList.text.Contains(item.Key + "✓")))
+            {
+                uITextQuestList.text = uITextQuestList.text.Replace(item.Key, item.Key + "✓");
+            }
+        }
+    }
+
 
     public static void TryToEscape()
     {
